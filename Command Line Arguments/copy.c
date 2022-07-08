7603b303e7e5c1bfa5562ff21a4f7ac83d9c9641
creative201347 <creative201347@gmail.com>
2022-07-08 19:54:29 +0545

    Command Line Arguments

---
+++ Command Line Arguments/copy.c
@@ -0,0 +1,51 @@
+/*
+Copying file
+    <src> : name of source file to be copied
+    <destination>: name of destination file
+
+    fgets(): Read a character from STREAM.
+    fputs(): Write a character to STREAM
+    int ch;
+    while ((ch = fgetc(src)) != EOF)
+        fputc(ch, dst);
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 3)
+    {
+        printf("Usage: copy <src> <destination>\n");
+        exit(1);
+    }
+
+    char *source_filename = argv[1];
+    char *dest_filename = argv[2];
+    FILE *src, *dst;
+
+    src = fopen(source_filename, "r");
+    if (src == NULL)
+    {
+        printf("The file %s does not exist.\n", source_filename);
+        exit(2);
+    }
+    dst = fopen(dest_filename, "w");
+    if (dst == NULL)
+    {
+        printf("The file %s cannot be created.\n", dest_filename);
+        fclose(src);
+        exit(3);
+    }
+
+    int ch;
+    while ((ch = fgetc(src)) != EOF)
+        fputc(ch, dst);
+
+    fclose(src);
+    fclose(dst);
+    return 0;
+}
